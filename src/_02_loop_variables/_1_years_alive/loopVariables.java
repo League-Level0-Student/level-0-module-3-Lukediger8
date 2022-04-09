f7ed2cc9bb162da1709e51900b46913e2897c942
league <league@imac-24.attlocal.net>
2022-04-09 12:55:32 -0700

    a

---
+++ src/_02_loop_variables/_1_years_alive/loopVariables.java
@@ -0,0 +1,29 @@
+package _02_loop_variables._1_years_alive;
+
+public class loopVariables {
+	public static void main(String[] args) {
+
+		for ( int year = 2011; year <= 2022; year++){
+		
+		System.out.println("the year is "+year);
+		
+		
+		
+		
+		
+		}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+}
