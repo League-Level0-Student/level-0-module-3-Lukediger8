e2fdd449cc6ff607cc7d8359bd778981f3106a99
league <league@imac-24.attlocal.net>
2022-04-09 12:54:56 -0700

    a

--- src/_01_else_if/_5_own_adventure/elseownadventure.java
+++ src/_01_else_if/_5_own_adventure/elseownadventure.java
@@ -1,50 +1,62 @@
 package _01_else_if._5_own_adventure;
 
+import java.net.URI;
+
 import javax.swing.JOptionPane;
 
 public class elseownadventure {
-public static void main(String[] args) {
-	
-	
-	
-	JOptionPane.showMessageDialog(null, "Welcome to One Piece ");
-	
-	String answer = JOptionPane.showInputDialog(null,"Are you ready for the journey?");
-	
-	if (answer.equals("yes")) {
-		JOptionPane.showMessageDialog(null, "alright, for your first misssion you will have to go into territory and steal their gold");
-		String answer2 = JOptionPane.showInputDialog(null,"are you up for the task");
-		if (answer2.equals("yes")) {
-			String answer3=JOptionPane.showInputDialog(null,"you have encountered a monster from the enemy territory, would you like to kill it?");
-			
-			if (answer3.equals("yes")) {
-			String attack = JOptionPane.showInputDialog("what move would you like to do");
-			if (attack.equals("spinning kick")) {
-				JOptionPane.showInputDialog(null,"the creature is down to %75, what move do you want to do next ");
-				if (attack.equals("dragon punch")) {
-					JOptionPane.showInputDialog(null,"the creature is down to %50, what move would you like to do next?");
+	public static void main(String[] args) {
+
+		JOptionPane.showMessageDialog(null, "Welcome to One Piece ");
+
+		String answer = JOptionPane.showInputDialog(null, "Are you ready for the journey?");
+
+		if (answer.equals("yes")) {
+			JOptionPane.showMessageDialog(null,
+					"alright, for your first misssion you will have to go into territory and steal their gold");
+			String answer2 = JOptionPane.showInputDialog(null, "are you up for the task");
+			if (answer2.equals("yes")) {
+				String answer3 = JOptionPane.showInputDialog(null,
+						"you have encountered a monster from the enemy territory, would you like to kill it?");
+
+				if (answer3.equals("yes")) {
+					String attack = JOptionPane.showInputDialog("what move would you like to do");
+					if (attack.equals("spinning kick")) {
+						attack = JOptionPane.showInputDialog(null,
+								"the creature is down to %75, what move do you want to do next ");
+
+					} else if (attack.equals("dragon punch")) {
+						JOptionPane.showInputDialog(null,
+								"the creature is down to %50, what move would you like to do next?");
+
+					} else if (attack.equals("galaxy isolation")) {
+						JOptionPane.showMessageDialog(null, "The creature has been defeated! Congratulations!");
+					}
+					String bury = JOptionPane.showInputDialog(null, "Would you like to bury the gold?");
+					if (bury.equals("yes")) {
+						JOptionPane.showMessageDialog(null,
+								"you planted the treasure in a mysterious island than your boat sailed away and you died from thirst and hunger.");
+					}
+					if (bury.equals("no")) {
+						JOptionPane.showMessageDialog(null, "your gold got stolen and you got killed");
+						JOptionPane.showMessageDialog(null, "TO BE CONTINUED...");
+					}
+						playVideo("https://www.youtube.com/watch?v=7uBqNgxAuBA");
 				}
+
+				if (answer.equals("no")) {
+					JOptionPane.showMessageDialog(null, "get outa here failure");
+				}
+				
 			}
-			
-			}
-			
-		}
-		
-		
-		
-if (answer.equals("no")) {
-		JOptionPane.showMessageDialog(null,"get outa here failure");	
 		}
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	static void playVideo(String videoURL) {
+		try {
+			URI uri = new URI(videoURL);
+			java.awt.Desktop.getDesktop().browse(uri);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 }
 }
--- src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
+++ src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
@@ -9,11 +9,21 @@ public class WalkOfFame {
 		
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
-
+		rob.setX(0);
+		rob.penDown();
+		rob.setSpeed(200);
+		for(int nStars=0; nStars<10;nStars++) {
+		for(int i=0; i<5; i++) {
+			rob.move(30);
+			rob.turn(144);
+		
+		}
+		rob.moveTo(50*(nStars+1), 250);
+		}
 		// 2. Make the robot draw a star shape. Hint: angle=144.
-
+		
 		// 3. Set the length of each line in the star to 30.
-
+	
 		/** THE CHALLENGE: **/
 		/*
 		 * Make the robot draw a line of 10 stars (see recipe to see how it should look)
